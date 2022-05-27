c50f967936e9aa277901d780131d52776ea91c27
Rowings <zakariaazarkan@gmail.com>
2022-05-27 14:49:07 +0200

    ajout animation bulle montante vers le haut

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ let size = Math.trunc(Math.random() * 200 + 100 ) + "px" ;
 
 bubble.style.height = size
 bubble.style.width = size
+bubble.style.top = Math.trunc(Math.random() * 100 + 50 ) + "%" ;
+bubble.style.left = Math.trunc(Math.random() * 100 ) + "%" ;
 
+// ici on a generer les positions des bulles 
 
 console.log(size);
